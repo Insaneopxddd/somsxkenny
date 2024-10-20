52616fb9f06cd379da80f82d7c35e0658762b6f1
Insaneopxddd <someshk2512@gmail.com>
2024-10-20 21:32:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @BLURYF4CE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7932726796:AAHkwY4FMttKejKo8M_TZWaeQlcJfOCu8So")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@SomsPvt & @KennyPvt")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7415482484, 1515732695")
+
+
+
+
+
+
+
+  
